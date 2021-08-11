073679ca55062d119785e6475de6700692e2e1b9
Eduard <stan.i.eduard@gmail.com>
2021-08-11 13:05:36 +0200

    threads moved from NSGAII/functions.jl to mmmc function

--- src/MRBC.jl
+++ src/MRBC.jl
@@ -6,7 +6,7 @@ using NSGAII
 using Random
 using StatsBase
 
-import Base: copyto!, eltype, isequal, iterate, getindex, length, ndims, size, show, hash
+import Base: copyto!, eltype, isequal, iterate, getindex, length, firstindex, lastindex, ndims, size, show, hash
 import StatsBase: sample
 
 export readARFF, sample, paa, transform!, CV, rules_crossover!, rules_mutation!, z, init, H
@@ -19,6 +19,8 @@ include("core_functions.jl")
 train = readARFF("data/RacketSports/RacketSports_TRAIN.arff");
 transform!(train,paa,1;n_chunks=5);
 
-H = Dict{UInt, Dict{UInt, Dict{Int, Dict{NTuple{2,Int}, Float64}}}}()
+# H = Dict{UInt, Dict{UInt, Dict{Int, Dict{NTuple{2,Int}, Float64}}}}()
+
+H = Dict{Tuple{UInt, UInt, UInt, Tuple{UInt, UInt}}, Float64}()
 
 end
--- src/run.jl
+++ src/run.jl
@@ -15,5 +15,9 @@ end
 
 @save "model.jld2" M
 
-# old version
+# old version: H = Dict{UInt, Dict{UInt, Dict{Int, Dict{NTuple{2,Int}, Float64}}}}()
 # 16.428 s (604568605 allocations: 12.88 GiB)
+
+# new version: H = Dict{Tuple{UInt, UInt, UInt, Tuple{UInt, UInt}}, Float64}()
+# 15.376 s (535684825 allocations: 11.40 GiB)
+# 10.489 s (435886655 allocations: 9.23 GiB)
