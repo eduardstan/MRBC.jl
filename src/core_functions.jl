c4c6aff346455b28d635cb630673bd9d9262a4f3
Eduard <stan.i.eduard@gmail.com>
2021-08-14 23:31:08 +0200

    log2(N) heuristic

--- src/core_functions.jl
+++ src/core_functions.jl
@@ -291,7 +291,7 @@ function _mc3(I::ModalInstance,
     sub = _collect_sorted_subformulas(φ)
     L = Dict{Tuple{UInt,NTuple{2,Int64}},Float64}()
     N = length(I, 1)  # TODO works only on the first (1) frame, i.e., time series
-    intervals = [(x,y) for x in 1:N for y in 1:N if x < y]
+    intervals = [(x,y) for x in 1:N for y in 1:N if x < y && y-x+1 ≤ log2(N)]
     for ψ in sub
 
         # # Subformula already checked?
@@ -787,7 +787,7 @@ function init()
     init_relations  = [exIntRel(:G),univIntRel(:G)]
     prop_relations  = [conjunction, implication]
     relations       = vcat(ex_relations, univ_relations,prop_relations)
-    _rand_rules(init_relations,relations,HeytingChainAlgebra,train,[≤,<,==,>,≥]; minh=1,maxh=3,maxmd=3,maxdepth=5,minnumrules=4,maxnumrules=12)
+    _rand_rules(init_relations,relations,HeytingChainAlgebra,train,[≤,<,==,>,≥]; minh=1,maxh=3,maxmd=4,maxdepth=6,minnumrules=4,maxnumrules=48)
 end
 
 function hypervolume_indicator(y::Vector{Tuple{Float64,Float64}}; refpoint=(0,0))
